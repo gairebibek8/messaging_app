cce8c57aeb3eb780e25b41fa29be562c160acffb
Bibek Gaire <gairebibek8@gmail.com>
2023-11-08 00:14:59 -0600

    Accessing single chat data with the help of id

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,26 @@
 const express = require("express");
+const { chats } = require("./data/data");
 
 // instance of express
 const app = express();
 
 // making request and getting response
 app.get('/', (req, res) => {
-    res.send("API is running")
+    res.send("API is running");
 });
 
 
+// fetching data from data.js as export const chats is done
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
+
+// fetching particular chat only with the help of id
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
 
 
 
